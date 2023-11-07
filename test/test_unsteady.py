62559747d437d40922672c03c5806a0c76ed4456
Libor Kudela <libor.kudela@vutbr.cz>
2023-11-07 12:11:35 +0100

    Improving unsteady test

--- test/test_unsteady.py
+++ test/test_unsteady.py
@@ -1,16 +1,22 @@
 from heat_battery.simulations import Experiment
 import unittest
+import numpy as np
 
 class TestOptimization(unittest.TestCase):
     def setUp(self) -> None:
         self.sim = Experiment(dim = 2)
     
     def test_self_fitter(self):
+        T_amb = 20.0
+        Qc = 100.0
+        t_max = 100.0
+        T_amb_t = lambda t: T_amb
+        Qc_t = lambda t: Qc
+        res = self.sim.solve_unsteady(T_amb_t=T_amb_t, Qc_t=Qc_t, t_max=t_max)
+        stored_heat = self.sim.probes.get_value('heat')
 
-        T_amb_t = lambda t: 20.0
-        Qc_t = lambda t: 100.0
-        res = self.sim.solve_unsteady(T_amb_t=T_amb_t, Qc_t=Qc_t, t_max=100)
-        self.assertTrue(True, "Test did not fail")
+        # ignoring little bit of heat loss but t_max is small so it should be ok
+        self.assertTrue(np.isclose(stored_heat, Qc*t_max, atol=1e-5), "Stored heat is wrong")
 
     def tearDown(self) -> None:
         self.sim.close_results()
