29cd352219f14a6fd90ed494e80e6aa6258e54c5
Libor Kudela <libor.kudela@vutbr.cz>
2024-11-28 00:45:48 +0100

    Add simple import tests

---
+++ test/test_a_importing.py
@@ -0,0 +1,10 @@
+from heat_battery.simulations import Simulation
+import unittest
+
+class TestExamples(unittest.TestCase):
+
+    def test_selected_examples(self):
+        print(Simulation)
+
+if __name__ == '__main__':
+    unittest.main()
