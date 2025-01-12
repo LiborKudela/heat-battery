f711d7bf33c8de7f811b90f8b8c6d38077aa1f6d
Libor Kudela <libor.kudela@vutbr.cz>
2025-01-12 12:08:37 +0100

    Reworking github workflows

--- install_scripts/install_ubuntu.sh
+++ install_scripts/install_ubuntu.sh
@@ -2,62 +2,145 @@
 
 # Function to display help message
 Help() {
-    echo "Usage: $0 [-p install_postgres] [-ppass postgres_password]"
     echo
     echo "Install heat-battery package, its dependencies and setup PostgreSQL"
     echo
     echo "Options:"
-    echo "  -y       Automatically answer yes to all apt-get prompts"
-    echo "  -p       Also install and configure PostgreSQL"
-    echo "  -ppass   Set PostgreSQL password for postgres user"
-    echo "  -ppassc  Confirm PostgreSQL password for postgres user"
-    echo "  -hbdir   Set heat battery data directory (default: /home/username/heat_battery_data)"
+    echo "  -y        Automatically answer yes to all apt prompts"
+    echo "  -p        Also install and configure PostgreSQL"
+    echo "  --help    Show this help message"
+    echo "  --ppass   Set PostgreSQL password for postgres user"
+    echo "  --ppassc  Confirm PostgreSQL password for postgres user (must be the same as ppass)"
+    echo "  --hbdir   Set heat battery data directory (default: /home/username/heat_battery_data)"
     echo
     echo "IMPORTANT:"
-    echo "  The options -ppass and -ppassc are required when -p is set."
+    echo "  The options --ppass and --ppassc are required when -p is set."
     echo
     echo "Example:"
-    echo "  $0 -p -ppass mypassword -ppassc mypassword -hbdir /home/user/heat_battery_data"
+    echo "  $0 -y"
+    echo "  $0 -y -p --ppass mypassword --ppassc mypassword --hbdir /home/user/heat_battery_data"
     echo
     echo "The script will:"
     echo "  1. Update system packages"
     echo "  2. Install and test OpenMPI"
     echo "  3. Install and test Gmsh"
     echo "  4. Install FEniCSx"
-    echo "  5. Install and configure PostgreSQL if -p is set"
-    echo "  6. Install heat-battery package"
+    echo "  5. Build, install and test ADIOS2 with MPI enabled"
+    echo "  6. Install and configure PostgreSQL if -p is set"
+    echo "  7. Install heat-battery python3 module"
+    exit 0
 }
-# default arguments
+
+# default arguments values
 user_name=$(whoami)
 auto_yes=""
-install_postgres=0
+auto_yes_bool=false
+install_postgres=false
 postgres_password=""
 postgres_password_confirm=""
 heat_battery_data_dir="home/$user_name/heat_battery_data"
 
-#parse bash named arguments
-while getopts ":p:ppass:ppassc:hbdir:" opt; do
-    case $opt in
-        y ) auto_yes="-y";;
-        p ) install_postgres=1;;
-        ppass ) postgres_password=$OPTARG;;
-        ppassc ) postgres_password_confirm=$OPTARG;;
-        hbdir ) heat_battery_data_dir=$OPTARG;;
-        \? ) Help; exit 1;;
-    esac
+# parse options
+VALID_ARGS=$(getopt -o yp --long ppass:,ppassc:,hbdir: -- "$@")
+
+# exit if getopt fails
+if [ $? -ne 0 ]; then
+    Help
+    exit 1;
+fi
+
+# count non option arguments
+no_option_args=0
+skip_next=false; 
+for arg in $VALID_ARGS; do
+    if [[ "$arg" = -- ]]; then
+        skip_next=false;
+    elif [[ "$arg" = --* ]]; then
+        skip_next=true;
+    elif [[ "$arg" = -* ]]; then
+        skip_next=false;
+        :
+    else
+        if [[ $skip_next = false ]]; then
+            no_option_args=$((no_option_args + 1))
+            skip_next=false;
+        else
+            :
+        fi
+    fi
+done
+
+# # exit if there are positional arguments 
+# coment this block if you want to use positional arguments as input ect.
+if [ $no_option_args -gt 0 ]; then
+    echo "Error: Arguments that are not defined in the help are not allowed."
+    Help
+    exit 1
+fi
+
+# make sure we run the fresh set from getopt
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+  case "$1" in
+    -y)
+        auto_yes="-y"
+        auto_yes_bool=true
+        shift
+        ;;
+    -p)
+        install_postgres=true
+        shift
+        ;;
+    --help)
+        Help
+        ;;
+    --ppass)
+        postgres_password=$2
+        shift 2
+        ;;
+    --ppassc)
+        postgres_password_confirm=$2
+        shift 2
+        ;;
+    --hbdir)
+        heat_battery_data_dir=$2
+        shift 2
+        ;;
+    --) shift; 
+        break 
+        ;;
+  esac
 done
 
 # check if arguments are valid
-if [ "$install_postgres" = "1" ] then
+if $install_postgres; then
+    echo "ps set"
     if [ "$postgres_password" != "" ]; then
         if [ "$postgres_password" != "$postgres_password_confirm" ]; then
             echo "PostgreSQL password and confirmation password do not match! Exiting.."
+            Help;
             exit 1
         fi
     else
         echo "PostgreSQL password is required when -p is set! Exiting.."
+        Help;
         exit 1
     fi
+fi
+
+echo "Running script with options:"
+echo "  -y: $auto_yes_bool"
+echo "  -p: $install_postgres"
+echo "  --ppass: $postgres_password"
+echo "  --ppassc: $postgres_password_confirm"
+echo "  --hbdir: $heat_battery_data_dir"
+
+echo "Inputs: $no_option_args"
+if [ $no_option_args -gt 0 ]; then
+    echo "Inputs:"
+    for arg in "$@"; do
+        echo "  $arg"
+    done
 fi
 
 #check if heat_batter_data_dir parent directory exists when default is used
@@ -73,11 +156,12 @@ echo "Provided argumets are valid, starting installation..."
 
 # update system and ensure essential packages are installed
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade $auto_yes
 echo "APT available packages list updated!"
 sudo apt install build-essential $auto_yes
 sudo apt install cmake $auto_yes
 sudo apt install git $auto_yes
+sudo apt install python3-dev $auto_yes
 sudo apt install python3-pip $auto_yes
 echo "Build-essential and python3-pip installed!"
 
@@ -132,7 +216,7 @@ if ! python3 -c "import adios2; print(f'adios2 version: {adios2.__version__}')";
     p3_corect_dir="/usr/local/lib/python${PY_VERSION}/dist-packages/adios2"
     echo "Python adios2 import failed - copying binding directly to ${p3_corect_dir}"
     sudo mkdir -p ${p3_corect_dir}
-    sudo cp -r /lib/python3/dist-packages/adios2* ${p3_corect_dir}/
+    sudo cp -r lib/python3/dist-packages/adios2/* ${p3_corect_dir}/
     sudo ldconfig
     echo "Trying again python import again..."
     if ! python3 -c "import adios2; print(f'adios2 version: {adios2.__version__}')"; then
@@ -175,6 +259,7 @@ if [ "$install_postgres" = "true" ]; then
         echo "   - service (for WSL Ubuntu)"
         exit 1
     fi
+    sudo pg_isready
     echo "PostgreSQL server started successfully!"
 
     # set password for postgres user
@@ -184,6 +269,7 @@ if [ "$install_postgres" = "true" ]; then
 
     # set permissions for postgres user
     echo "Setting permissions for postgres user..."
+    sudo mkdir -p $heat_battery_data_dir
     sudo setfacl -Rm u:postgres:rwx,u:$(whoami):rwx $heat_battery_data_dir
     sudo setfacl -Rdm u:postgres:rwx,u:$(whoami):rwx $heat_battery_data_dir
     echo "Permissions for postgres user set successfully!"
