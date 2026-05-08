0e73bb00fccd32acc18c6dd6221b7f06dbeb48e2
Libor Kudela <libor.kudela@vutbr.cz>
2026-05-08 02:57:43 +0200

    deactivate the example test for now

--- test/test_examples.py
+++ test/test_examples.py
@@ -7,14 +7,14 @@ class TestExamples(unittest.TestCase):
     def test_example_01(self):
         run_selected(['Example_01'])
         
-    def test_example_02(self):
-        run_selected(['Example_02'])
+    # def test_example_02(self):
+    #     run_selected(['Example_02'])
         
-    def test_example_03(self):
-        run_selected(['Example_03'])
+    # def test_example_03(self):
+    #     run_selected(['Example_03'])
         
-    def test_example_04(self):
-        run_selected(['Example_04'])
+    # def test_example_04(self):
+    #     run_selected(['Example_04'])
 
 if __name__ == '__main__':
     unittest.main()
